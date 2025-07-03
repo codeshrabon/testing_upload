f4bc4493a22f52d680eff56c0d3d856ad5a1ecc4
Faysal Ahmed <19101043@uap-bd.edu>
2025-07-02 17:10:42 -0700

    after git suthentication

---
+++ App.java
@@ -0,0 +1,9 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Hello, World!");
+
+        NewClass obj = new NewClass();
+        // using object we can interact with method of the class
+        obj.info();
+    }
+}
---
+++ NewClass.java
@@ -0,0 +1,13 @@
+public class NewClass {
+
+    NewClass(){
+        System.out.println("Using constructor");
+        System.out.println("No need to create any object for it");
+        System.out.println("autometically it will be called ");
+    }
+    void info(){
+        System.out.println("From New Class");
+        System.out.println("Faysal Ahmed");
+    }
+    
+}
